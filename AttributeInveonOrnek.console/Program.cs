c83f0d7dfb1d5ca3ce539b19eb7dda726568c949
durmazoguzhan <durmazoguzhan@yahoo.com>
2023-10-26 21:30:28 +0300

    init

---
+++ AttributeInveonOrnek.console/Program.cs
@@ -0,0 +1,17 @@
+﻿using AttributeInveonOrnek.console;
+
+
+Ogrenci ogr = new Ogrenci();
+ogr.adi = "Oğuzhan";
+ogr.soyadi = "Durmaz";
+ogr.bolum = "Yazılım";
+
+
+if (!ZorunlulukKontrolu.Dogrula(ogr))
+{
+    Console.WriteLine("Öğrenci bilgileri girilmesi zorunludur.");
+}
+else
+{
+    Console.WriteLine("Form başarılı");
+}
